37a86298d9c6b094f5fdda3f553c0f5919adc38f
王 偉澔 <drxk41012@gmail.com>
2018-03-07 00:33:06 -0500

    wifiSpeedTest

---
+++ wifi speed/wifiSpeed.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	var imageAddr = "wifiSpeed/images/test.jpg" ;
+	var startTime, endTime;
+	var downloadSize = 200000;
+	var download = new Image();
+
+	download.onload = function () {
+	    endTime = (new Date()).getTime();
+	    showResults();
+	}
+	startTime = (new Date()).getTime();
+	download.src = imageAddr;
+	
+	function showResults() {
+	    var duration = (endTime - startTime) / 1000;
+	    var bitsLoaded = downloadSize * 8;
+	    var speedBps = (bitsLoaded / duration);
+	    alert("Your connection speed is: \n" + 
+	           speedBps + " bps\n"   + 
+	           speedKbps + " kbps\n" + 
+	           speedMbps + " Mbps\n" );
+	}
+	
+});
+
