6fc2b9cfaba47022a110f10ee7d701dbd2ca6467
王 偉澔 <drxk41012@gmail.com>
2018-03-07 00:48:00 -0500

    we

--- wifiSpeed/wifiSpeed.js
+++ wifiSpeed/wifiSpeed.js
@@ -4,13 +4,6 @@ $(document).ready(function(){
 	var downloadSize = 200000;
 	var download = new Image();
 
-	download.onload = function () {
-	    endTime = (new Date()).getTime();
-	    showResults();
-	}
-	startTime = (new Date()).getTime();
-	download.src = imageAddr;
-	
 	function showResults() {
 	    var duration = (endTime - startTime) / 1000;
 	    var bitsLoaded = downloadSize * 8;
@@ -18,6 +11,12 @@ $(document).ready(function(){
 	    alert("Your connection speed is: \n" + 
 	           speedBps + " bps\n");
 	}
-	
+
+	download.onload = function () {
+	    endTime = (new Date()).getTime();
+	    showResults();
+	}
+	startTime = (new Date()).getTime();
+	download.src = imageAddr;
 });
 
